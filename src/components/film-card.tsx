2eac17971303d21af0ee3e1e3028ae6f3ae11328
Vladislav Maslakov <maslakov20@mail.ru>
2023-10-12 13:45:01 +0500

    Fix bugs with &apos;

--- src/components/film-card.tsx
+++ src/components/film-card.tsx
@@ -5,23 +5,21 @@ export type FilmProps = {
 
 function FilmCard({srcCard, filmName}: FilmProps): JSX.Element {
   return (
-    <>
-      <article className="small-film-card catalog__films-card">
-        <div className="small-film-card__image">
-          <img
-            src={srcCard}
-            alt={filmName}
-            width={280}
-            height={175}
-          />
-        </div>
-        <h3 className="small-film-card__title">
-          <a className="small-film-card__link" href="film-page.html">
-            {filmName}
-          </a>
-        </h3>
-      </article>
-    </>
+    <article className="small-film-card catalog__films-card">
+      <div className="small-film-card__image">
+        <img
+          src={srcCard}
+          alt={filmName}
+          width={280}
+          height={175}
+        />
+      </div>
+      <h3 className="small-film-card__title">
+        <a className="small-film-card__link" href="film-page.html">
+          {filmName}
+        </a>
+      </h3>
+    </article>
   );
 }
 
